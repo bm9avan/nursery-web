d430a6e2e41caa94d3efb133053130cdca28741b
bm9avan <mggpavan@gmail.com>
2023-01-24 19:08:32 +0530

    adding footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import '@/styles/globals.css'
 import Nav from '../components/navBar'
+import Foot from '../components/footer'
 
 export default function App({ Component, pageProps }) {
-  
-  
+
+
   return <>
-  <Nav/>
-  <Component {...pageProps} />
+    <Nav />
+    <div className="min-h-screen grid grid-rows-1">
+      <div className='m-5'>
+      <Component {...pageProps}  />
+      </div>
+      <div className='bg-g-100'>
+      <Foot className=' absolute  top-0 w-full mt-5' />
+      </div>
+    </div>
   </>
 }
