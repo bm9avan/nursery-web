f52b7c2a18525bef0ef8ca6af488fe046c125723
bm9avan <mggpavan@gmail.com>
2023-02-21 11:51:31 +0530

    product api completed

--- pages/api/addProduct.js
+++ pages/api/addProduct.js
@@ -2,14 +2,9 @@ import Product from "@/models/Product"
 import connecyDb from "../../middleware/mongoose"
 
 async function handler(req, res) {
-  let product = await Product.find()
-  console.log(product)
-  console.log("step1: out")
   if (req.method == 'POST') {
-    console.log("step2: in if")
     try {
       for (let i = 0; i < req.body.length; i++) {
-        console.log("step3+"+i)
         const p = new Product({
           productId: req.body[i].productId,
           title: req.body[i].title,
@@ -20,11 +15,7 @@ async function handler(req, res) {
           amount: req.body[i].amount,
           availableQty: req.body[i].availableQty,
         })
-        console.log("step4: incomplete")
-        console.log(product)
-        console.log(p)
-        p.save();
-        console.log("step5: complete")
+        await p.save();
       }
       res.status(200).json({ sucess: "data added" })
     } catch {
---
+++ pages/api/updateProducts.js
@@ -0,0 +1,33 @@
+import Product from "@/models/Product"
+import connecyDb from "../../middleware/mongoose"
+
+async function handler(req, res) {
+  if (req.method == 'GET') {
+    try {
+      for (let i = 0; i < req.body.length; i++) {
+        console.log("before")
+        const p= await Product.updateOne({slug: req.body[i].slug},{$set:{
+          productId: req.body[i].productId,
+          title: req.body[i].title,
+          slug: req.body[i].slug,
+          img: req.body[i].img,
+          desc: req.body[i].desc,
+          categoty: req.body[i].categoty,
+          amount: req.body[i].amount,
+          availableQty: req.body[i].availableQty,
+        }},{upsert: true})
+        console.log(p)
+        console.log("after not saved")
+      // let p = await Product.updateOne({ "img": "changed" }, { $set: { img: "test", desc: "same", categoty: "changed" } }, { upsert: true })
+      console.log("after saved")
+    }
+      res.status(200).json({ sucess: "data added" })
+  } catch {
+    (e) => console.log("in catch ", e)
+  }
+} else {
+  res.status(400).json({ error: "this method is invalied" })
+}
+}
+
+export default connecyDb(handler)
